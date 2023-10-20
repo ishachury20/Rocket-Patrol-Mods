00612475d0988ef8e57d68ef73b9932ab9f22909
“ichury” <ichury@ucsc.edu>
2023-10-20 02:20:00 -0700

    added files

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -4,6 +4,8 @@ class Rocket extends Phaser.GameObjects.Sprite{
         scene.add.existing(this); 
         this.isFiring = false; 
         this.moveSpeed = 2; 
+        this.sfxRocket = scene.sound.add('sfx_rocket'); 
+        
     }
 
     update() {
@@ -16,6 +18,7 @@ class Rocket extends Phaser.GameObjects.Sprite{
         }
         if(Phaser.Input.Keyboard.JustDown(keyF)){
             this.isFiring = true; 
+            this.sfxRocket.play();
         }
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
             this.y -= this.moveSpeed; 
@@ -24,6 +27,11 @@ class Rocket extends Phaser.GameObjects.Sprite{
             this.isFiring = false; 
             this.y = game.config.height - borderUISize - borderPadding; 
         }
+    }
+
+    reset(){
+        this.isFiring = false; 
+        this.y = game.config.height - borderUISize - borderPadding; 
     }
 
 }
